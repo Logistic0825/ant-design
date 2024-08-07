6adc13170147e15d023d9177b2fb1f645d2a15e2
Logistic0825 <105987712+Logistic0825@users.noreply.github.com>
2024-08-07 13:56:52 +0800

    Update simple.tsx
    Signed-off-by: Logistic0825 <105987712+Logistic0825@users.noreply.github.com>

--- components/card/demo/simple.tsx
+++ components/card/demo/simple.tsx
@@ -4,8 +4,7 @@ import { Card } from 'antd';
 const App: React.FC = () => (
   <Card style={{ width: 300 }}>
     <p>Card content</p>
-    <p>Card content</p>
-    <p>Card content</p>
+    
   </Card>
 );
 
